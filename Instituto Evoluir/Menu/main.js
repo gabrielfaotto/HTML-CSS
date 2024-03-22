ae359427fee8d805365b8d1c2411142a84f2622d
Gabriel Faotto <152933744+gabrielfaotto@users.noreply.github.com>
2024-03-22 15:43:09 -0300

    Projeto do Instituo e Exercícios sobre Formulários

---
+++ Instituto Evoluir/Menu/main.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+     if (navLinkss.style.display == 'inline-block') {
+          navLinkss.style.display = 'none'
+     } else {
+          navLinkss.style.display = 'inline-block'
+     }
+  }
+
